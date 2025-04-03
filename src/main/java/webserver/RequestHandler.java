9be09eaf3f38e2cf9c5f40fafa3cda7a233feda0
MinseoSONG <mins041600@naver.com>
2025-04-03 14:57:43 +0900

    [feat] 요구사항 1: index.html 반환하기 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,10 +2,13 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class RequestHandler implements Runnable{
+public class RequestHandler implements Runnable {
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
@@ -16,16 +19,43 @@ public class RequestHandler implements Runnable{
     @Override
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String line = br.readLine(); // ex) GET /index.html HTTP/1.1
+            if (line == null) return;
+
+            String[] tokens = line.split(" ");
+            String method = tokens[0];
+            String url = tokens[1];
+
+            if (url.equals("/")) {
+                url = "/index.html";
+            }
+
+            Path filePath = Paths.get("./webapp" + url);
+
+            if (Files.exists(filePath)) {
+                byte[] body = Files.readAllBytes(filePath);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else {
+                response404Header(dos);
+            }
 
         } catch (IOException e) {
-            log.log(Level.SEVERE,e.getMessage());
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
         }
     }
 
